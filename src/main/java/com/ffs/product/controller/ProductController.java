37dfc653f23ad755dedde178102131383ea1a097
Choi Ihwa <chldlghk@gmail.com>
2024-03-18 21:53:35 +0900

    [UPDATE] 제품 Controller 주석 추가 및 권한 추

--- src/main/java/com/ffs/product/controller/ProductController.java
+++ src/main/java/com/ffs/product/controller/ProductController.java
@@ -8,6 +8,7 @@ import com.ffs.product.dto.UpdateUseYnRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -20,7 +21,11 @@ public class ProductController {
 
     private final ProductService productService;
 
+    /**
+     * 신규 제품 등록
+     */
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN,ROLE_CEO,ROLE_MANAGER')")
     public ResponseEntity<Object> registerNewProduct(@RequestBody @Valid RegisterProductRequest request) {
         Product product = productService.registerNewProduct(request);
         ProductResult result = ProductResult.builder().product(product).build();
@@ -28,24 +33,34 @@ public class ProductController {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @GetMapping("/all/{branchId}")
-    public ResponseEntity<Object> getAllProductByBranch(@PathVariable Long branchId) {
+    /**
+     * 지점에 해당하는 제품들 조회
+     */
+    @GetMapping("/{branch_id}")
+    public ResponseEntity<Object> getAllProductByBranch(@PathVariable("branch_id") Long branchId) {
         List<Product> productList = productService.getAllProductByBranchId(branchId);
         ProductResult result = ProductResult.builder().productList(productList).build();
 
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Object> getProductById(@PathVariable Long id) {
+    /**
+     *  제품 상세 정보 조회
+     */
+    @GetMapping("/{product_id}")
+    public ResponseEntity<Object> getProductById(@PathVariable("product_id") Long id) {
         Product product = productService.getProductById(id);
         ProductResult result = ProductResult.builder().product(product).build();
 
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Object> updateUseYn(@PathVariable Long id, @RequestBody @Valid UpdateUseYnRequest request) {
+    /**
+     * 제품 사용 여부 수정
+     */
+    @PutMapping("/{product_id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN,ROLE_CEO,ROLE_MANAGER')")
+    public ResponseEntity<Object> updateUseYn(@PathVariable("product_id") Long id, @RequestBody @Valid UpdateUseYnRequest request) {
         Product product = productService.updateProductUseYn(id, request);
         ProductResult result = ProductResult.builder().product(product).build();
 
