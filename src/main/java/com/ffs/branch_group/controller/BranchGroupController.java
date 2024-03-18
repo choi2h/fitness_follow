3a3b4caefaee051910dd2620d05ef229bb98c82e
Choi Ihwa <chldlghk@gmail.com>
2024-03-18 21:54:55 +0900

    [UPDATE] 지점 그룹 Controller 주석 및 권한 추가

--- src/main/java/com/ffs/branch_group/controller/BranchGroupController.java
+++ src/main/java/com/ffs/branch_group/controller/BranchGroupController.java
@@ -7,6 +7,7 @@ import com.ffs.branch_group.application.BranchGroupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -19,7 +20,11 @@ public class BranchGroupController {
 
     private final BranchGroupService branchGroupService;
 
+    /**
+     * 지점 그룹 등록
+     */
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Object> registerNewBranchGroup(@RequestBody @Valid RegisterBranchGroupRequest request) {
         BranchGroupInfo branchGroupInfo = branchGroupService.registerNewBranchGroup(request);
         BranchGroupResult response = BranchGroupResult.builder().branchGroup(branchGroupInfo).build();
@@ -27,6 +32,9 @@ public class BranchGroupController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    /**
+     * 모든 지점 그룹 조회
+     */
     @GetMapping
     public ResponseEntity<Object> getAllBranchGroup() {
         List<BranchGroupInfo> branchGroupInfoList = branchGroupService.getAllBranchGroup();
@@ -35,7 +43,11 @@ public class BranchGroupController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    /**
+     * 지점 상세 정보 조회
+    */
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Object> getBranchById(@PathVariable Long id) {
         BranchGroupInfo branchGroupInfo = branchGroupService.getBranchGroup(id);
         BranchGroupResult response = BranchGroupResult.builder().branchGroup(branchGroupInfo).build();
