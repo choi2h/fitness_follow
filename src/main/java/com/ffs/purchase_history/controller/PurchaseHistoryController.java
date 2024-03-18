91df560be5545cc92854be60c0e384732677843a
Choi Ihwa <chldlghk@gmail.com>
2024-03-18 21:53:20 +0900

    [UPDATE] 구매기록 Controller 주석 추가 및 권한 추가

--- src/main/java/com/ffs/purchase_history/controller/PurchaseHistoryController.java
+++ src/main/java/com/ffs/purchase_history/controller/PurchaseHistoryController.java
@@ -7,6 +7,7 @@ import com.ffs.purchase_history.dto.RegisterPurchaseHistoryRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -19,7 +20,11 @@ public class PurchaseHistoryController {
 
     private final PurchaseHistoryService purchaseHistoryService;
 
+    /**
+     * 구매 기록 등록
+     */
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN,ROLE_CEO,ROLE_MANAGER')")
     public ResponseEntity<Object> registerPurchaseHistory(@RequestBody @Valid RegisterPurchaseHistoryRequest request) {
         PurchaseHistory purchaseHistory = purchaseHistoryService.registerNewPurchaseHistory(request);
         PurchaseHistoryResult result = PurchaseHistoryResult.builder().purchaseHistory(purchaseHistory).build();
@@ -27,24 +32,36 @@ public class PurchaseHistoryController {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @GetMapping("/branch/{id}")
-    public ResponseEntity<Object> getPurchaseHistoryListByBranchId(@PathVariable Long id) {
+    /**
+     * 지점의 구매 기록 조회
+     */
+    @GetMapping("/branch/{branch_id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN,ROLE_CEO,ROLE_MANAGER')")
+    public ResponseEntity<Object> getPurchaseHistoryListByBranchId(@PathVariable("branch_id") Long id) {
         List<PurchaseHistory> purchaseHistoryList = purchaseHistoryService.getAllPurchaseHistoryByBranchId(id);
         PurchaseHistoryResult result = PurchaseHistoryResult.builder().purchaseHistoryList(purchaseHistoryList).build();
 
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @GetMapping("/user/{id}")
-    public ResponseEntity<Object> getPurchaseHistoryListByUserId(@PathVariable Long id) {
+    /**
+     * 회원의 구매 기록 조회
+     */
+    @GetMapping("/user/{user_id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN,ROLE_CEO,ROLE_MANAGER, ROLE_MEMBER')")
+    public ResponseEntity<Object> getPurchaseHistoryListByUserId(@PathVariable("user_id") Long id) {
         List<PurchaseHistory> purchaseHistoryList = purchaseHistoryService.getAllPurchaseHistoryByUserId(id);
         PurchaseHistoryResult result = PurchaseHistoryResult.builder().purchaseHistoryList(purchaseHistoryList).build();
 
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Object> getPurchaseHistoryById(@PathVariable Long id) {
+    /**
+     * 구매기록 상세 조회
+     */
+    @GetMapping("/{purchase_id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN,ROLE_CEO,ROLE_MANAGER, ROLE_MEMBER')")
+    public ResponseEntity<Object> getPurchaseHistoryById(@PathVariable("purchase_id") Long id) {
         PurchaseHistory purchaseHistory = purchaseHistoryService.getPurchaseHistoryById(id);
         PurchaseHistoryResult result = PurchaseHistoryResult.builder().purchaseHistory(purchaseHistory).build();
 
